0024ad582c82c84f807478ef32bdc6fb31f4e7c9
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-16 09:57:00 -0400

    updated seed

--- employee_db.sql
+++ employee_db.sql
@@ -37,7 +37,7 @@ id INT AUTO_INCREMENT NOT NULL,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
-manager_id INT default NULL,
+manager_id INT,
 PRIMARY KEY (id)
 );
 
